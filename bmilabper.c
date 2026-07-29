1d11e2e1ad7f2688b67b2a96a36391632f4aff9c
raselchy845-a11y <raselchy845@gmail.com>
2026-07-29 13:29:26 +0600

    Create bmilabper.c

---
+++ bmilabper.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+int main()
+{
+    float bmi;
+    char category;
+
+    printf("Enter BMI: ");
+    scanf("%f", &bmi);
+
+    if (bmi < 0)
+        category = 'X';
+    else if (bmi < 18.5)
+        category = 'U';
+    else if (bmi < 25.0)
+        category = 'N';
+    else if (bmi < 30.0)
+        category = 'O';
+    else
+        category = 'B';
+
+    switch (category)
+    {
+    case 'U':
+        printf("Underweight");
+        break;
+
+    case 'N':
+        printf("Normal Weight");
+        break;
+
+    case 'O':
+        printf("Overweight");
+        break;
+
+    case 'B':
+        printf("Obese");
+        break;
+
+    case 'X':
+        printf("Invalid BMI");
+        break;
+
+    default:
+        printf("Unknown Category");
+    }
+
+    return 0;
+}
